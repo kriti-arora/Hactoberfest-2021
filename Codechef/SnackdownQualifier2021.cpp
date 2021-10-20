c3421d0646b93b99efdec2f913fcb68f0a6c1273
kriti-arora <92243363+kriti-arora@users.noreply.github.com>
2021-10-21 00:05:47 +0530

    Create SnackdownQualifier2021.cpp

---
+++ Codechef/SnackdownQualifier2021.cpp
@@ -0,0 +1,25 @@
+// Program for the 'Lucky Number' problem asked in Snackdown Qualifiers 2021
+//By Kriti Arora
+// UIET KURUKSHETRA
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int T;
+	cin>>T;
+	
+	while(T--){
+		int A,B,C;
+		cin>>A>>B>>C;
+		
+		if(A==7||B==7||C==7){
+			cout<<"YES"<<endl;
+		}
+		else{
+			cout<<"NO"<<endl;
+		}
+ }
+	
+	return 0;
+}
